05b582adabf389b555746b1ad385e1de0d660c0b
Matt Hale <mhale@idealintegrations.net>
2025-03-17 13:51:06 -0400

    Change parametrization away from fixture

--- tests/test_using_pytest.py
+++ tests/test_using_pytest.py
@@ -1,17 +1,17 @@
 import pytest
 import using_pytest
 
-@pytest.fixture(params=[
-    (1, 2),
-    (4, 6),
+@pytest.mark.parametrize('x,y,expectation', [
+    (1, 2, 3,),
+    (5, 10, 15,),
 ])
-def data(request):
-    return request.param
+def test_add(x, y, expectation):
+    assert using_pytest.add(x, y) == expectation
 
-def test_add(data):
-    x, y = data
-    assert using_pytest.add(x, y) == x + y
-
-def test_multiply(data):
-    x, y = data
-    assert using_pytest.multiply(x, y) == x * y
+@pytest.mark.parametrize('x,y,expectation', [
+    (1, 2, 2,),
+    (4, 6, 24,),
+    (10, 10, 100,),
+])
+def test_multiply(x, y, expectation):
+    assert using_pytest.multiply(x, y) == expectation
